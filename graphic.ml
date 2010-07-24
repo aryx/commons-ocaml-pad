30b88d50f57e0b32407c1cdf90cdb772f934b257
pad <pad@facebook.com>
2010-07-24 14:11:51 -0700

    initial commit, from darcs

---
+++ graphic.ml
@@ -0,0 +1,17 @@
+open Common
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+
+(* cf also my h_visualization containing color.ml, treemap.ml. etc *)
+
+(* alternatives:
+ *  - Graphics
+ *  - gtk drawing
+ *  - opengl (cf opengl.ml)
+ *  - SDL
+ *)
+
+
+
